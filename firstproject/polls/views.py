1b1b9e7cf9f7cebc49264eef1ae2b88733b3981b
soly <yds9826@naver.com>
2020-12-22 21:25:03 +0900

    ｐｙｔｈｏｎ　ｖｉｅｗ작업　모듈낫파운더에려

--- firstproject/polls/views.py
+++ firstproject/polls/views.py
@@ -2,15 +2,33 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader
+
+from firstproject.polls.models import Question
+
+
 def index(request):
-    return HttpResponse("왔노라, 보았노라, 이겼노라");
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def detail(request, question_id):
-    return HttpResponse("you're looking at quetion %s" %question_id)
+    return HttpResponse("you're looking at question %s" % question_id)
+
 
 def results(request, question_id):
     response = "you're looking at the results of question %s"
     return HttpResponse(response % question_id)
 
+
 def vote(request, question_id):
     return HttpResponse("you're voting on question %s" % question_id)
+
+
+def sol(request, test_str):
+    str = "test value %s"
+    return HttpResponse(str % test_str)
